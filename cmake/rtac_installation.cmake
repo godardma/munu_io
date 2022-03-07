4baee3ff1cafacbdb4ad33c309a092edb7400f64
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2022-03-07 15:52:56 +0100

    [cmake] revert some changes

--- cmake/rtac_installation.cmake
+++ cmake/rtac_installation.cmake
@@ -5,27 +5,30 @@ function(rtac_install_target TARGET_NAME)
         ADDITIONAL_CONFIG_COMMANDS)
 	cmake_parse_arguments(RTAC_INSTALLATION "${options}" "${oneValueArgs}"
 	                      "${multiValueArgs}" ${ARGN} )
-    
+
     # message(STATUS "=== ${RTAC_INSTALLATION_ADDITIONAL_CONFIG_COMMANDS}")
     set(RTAC_INSTALLATION_ADDITIONAL_CONFIG_BODY "")
     foreach(command ${RTAC_INSTALLATION_ADDITIONAL_CONFIG_COMMANDS})
         # message(STATUS "  === ${command}")
-        set(RTAC_INSTALLATION_ADDITIONAL_CONFIG_BODY 
+        set(RTAC_INSTALLATION_ADDITIONAL_CONFIG_BODY
             "${RTAC_INSTALLATION_ADDITIONAL_CONFIG_BODY}${command}\n")
     endforeach()
 
-    # RPATH related configuration (see https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/RPATH-handling for details)
-    set_target_properties(${TARGET_NAME} PROPERTIES
-        SKIP_BUILD_RPATH FALSE
-        BUILD_WITH_INSTALL_RPATH FALSE
-        INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib"
-        INSTALL_RPATH_USE_LINK_PATH TRUE
-    )
-    list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
-    if("${isSystemDir}" STREQUAL "-1")
+    get_target_property(TARGET_TYPE_VALUE ${TARGET_NAME} TYPE)
+    if(NOT ${TARGET_TYPE_VALUE} STREQUAL "INTERFACE_LIBRARY")
+        # RPATH related configuration (see https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/RPATH-handling for details)
         set_target_properties(${TARGET_NAME} PROPERTIES
-            INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib" # redundant with above ??
+            SKIP_BUILD_RPATH FALSE
+            BUILD_WITH_INSTALL_RPATH FALSE
+            INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib"
+            INSTALL_RPATH_USE_LINK_PATH TRUE
         )
+        list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+        if("${isSystemDir}" STREQUAL "-1")
+            set_target_properties(${TARGET_NAME} PROPERTIES
+                INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib" # redundant with above ??
+            )
+        endif()
     endif()
 
 	include(GNUInstallDirs)
@@ -34,7 +37,7 @@ function(rtac_install_target TARGET_NAME)
 	set(PROJECT_CONFIG "${CMAKE_CURRENT_BINARY_DIR}/generated/${TARGET_NAME}Config.cmake")
 	set(TARGET_EXPORT_NAME "${TARGET_NAME}Targets")
 	set(CONFIG_INSTALL_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/${TARGET_NAME}")
-	
+
 	include(CMakePackageConfigHelpers)
 	write_basic_package_version_file(
 	    "${VERSION_CONFIG}" COMPATIBILITY SameMajorVersion
@@ -48,7 +51,7 @@ function(rtac_install_target TARGET_NAME)
     # get_target_property(HEADERS ${TARGET_NAME} PUBLIC_HEADER)
     # message(STATUS "Public headers : ${HEADERS}")
 
-	
+
 	# Installation
 	install(
 	    TARGETS "${TARGET_NAME}"
@@ -70,7 +73,11 @@ function(rtac_install_target TARGET_NAME)
 	)
 
     # Installing header files
-    get_target_property(HEADER_FILES ${TARGET_NAME} RTAC_PUBLIC_HEADERS)
+    if(${TARGET_TYPE_VALUE} STREQUAL "INTERFACE_LIBRARY")
+        get_target_property(HEADER_FILES ${TARGET_NAME} INTERFACE_PUBLIC_HEADER)
+    else()
+        get_target_property(HEADER_FILES ${TARGET_NAME} RTAC_PUBLIC_HEADERS)
+    endif()
     if(NOT "${HEADER_FILES}" STREQUAL "HEADER_FILES-NOTFOUND")
         # message(STATUS "HEADER_FILES : ${HEADER_FILES}")
         foreach(header ${HEADER_FILES})
